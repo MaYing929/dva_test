7cbcc50481fb1e32b02735d0f97a74ab7268fa92
ma ying <ying.ma@meishisong.cn>
2016-08-30 22:26:53 +0800

    correct showloading

--- src/models/users.js
+++ src/models/users.js
@@ -1,64 +1,63 @@
-import { hashHistory } from 'dva/router';
+
+
+import { hashHistory} from 'dva/router';
+
 import { query } from '../services/users';
 
-
 export default {
-  namespace:'users',
+
+  namespace: 'users',
 
   state: {
-    list:[],
-    total:null,
-    loading:false, //控制加载状态
-    current:null,  //当前分页信息
-    currentItem:{},   //当前操作的用户对象
-    modalVisible :false, //弹出框的显示状态
-    modalType:'create' //弹出框的类型（添加用户，编辑用户）
+    list: [],
+    loading: false,
+    total: null,
+    current: 1,
+    currentItem: {},
+    modalVisible: false,
+    modalType: 'create'
+  },
 
-  },
   subscriptions: {
-    setup({ dispatch,history }){
-       history.listen(location =>{
-         if(location.pathname === '/users'){
-           dispatch({
-             type:'querySuccess', //调用哪一个action
-             payload:{}  //调用时传递的参数
-           })
-         }
-       })
-    }
-  },
-  effects: {
-    // 简单理解 call 是调用执行一个函数而 put 则是相当于 dispatch 执行一个 action，而 select 则可以用来访问其它 model
-    *query({ payload }, { select, call, put }) {
-    yield put({ type: 'showLoading' });
-    const { data } = yield call(query);
-    if (data) {
-      yield put({
-        type: 'querySuccess1',
-        payload: {
-          list: data.data,
-          total: data.page.total,
-          current: data.page.current
+    setup({ dispatch,history }) {
+      history.listen(location => {
+        if (location.pathname === '/users') {
+          dispatch({
+            type: 'query',
+            payload: location.query  //{} 不传递参数的时候
+          });
         }
       });
-    }
-  },
-    *create(){},
-    *'delete'(){},
-    *update(){},
-  },
-  reducers: {
-    // showLoading(state, action){
-    //   return { ...state, loading: true }
-    // }, //控制加载状态的reducer
-    showModal(){}, //控制modal显示状态的reducer
-    hideModal(){},
-    //使用静态数据返回
-    querySuccess(state, action){
-    return {...state, ...action.payload, loading: true};
     },
-    createSuccess(){},
-    deleteSuccess(){},
-    updateSuccess(){},
+  },
+
+  effects: {
+
+ // 简单理解 call 是调用执行一个函数而 put 则是相当于 dispatch 执行一个 action，而 select 则可以用来访问其它 model
+    *query({ payload }, { select, call, put }) {
+          yield put({ type: 'showLoading' });
+          const { data } = yield call(query);
+          if (data) {
+            yield put({
+              type: 'querySuccess',
+              payload: {
+                list: data.data,
+                total: data.page.total,
+                current: data.page.current
+              }
+            });
+          }
+        },
+
+  },
+
+  reducers: {
+    showLoading(state) {
+      return { ...state, loading: true };
+    },
+    querySuccess(state, action) {
+      return { ...state, ...action.payload, loading: false };
+    },
+
   }
-}
+};
