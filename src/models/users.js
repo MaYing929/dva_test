413a424472d07f14ea63b9d3d3df0f0010c943c6
ma ying <ying.ma@meishisong.cn>
2016-08-30 21:39:29 +0800

    add service effects on one page

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,15 @@
 import { hashHistory } from 'dva/router';
 
+// 处理异步请求
+import  request from '../utils/request';
+import qs from 'qs';
+
+ async function query(params){
+   console.log('params',params)
+   return request(`/api/users?${qs.stringify(params)}`)
+ }
+
+
 export default {
   namespace:'users',
 
@@ -29,40 +39,56 @@ export default {
     }
   },
   effects: {
-    *query(){},
+    // 简单理解 call 是调用执行一个函数而 put 则是相当于 dispatch 执行一个 action，而 select 则可以用来访问其它 model
+    *query({payload}, {select, all, put}){
+      yield put({type:'showLoading'})
+      const { data } = yield call(query);
+      if(data) {
+        yield put ({
+          type: 'querySuccess',
+          payload: {
+            list:data.data,
+            total:data.page.total,
+            current:data.page.current
+          }
+        });
+      }
+    },
     *create(){},
     *'delete'(){},
     *update(){},
   },
   reducers: {
-    showLoading(){}, //控制加载状态的reducer
+    showLoading(state, action){
+      return {...state, loading: true}
+    }, //控制加载状态的reducer
     showModal(){}, //控制modal显示状态的reducer
     hideModal(){},
     //使用静态数据返回
-    querySuccess(state){
-     const mock = {
-        total: 3,
-        current: 1,
-        loading: false,
-        list: [
-          {
-            name: '张三',
-            age: 23,
-            address: '成都',
-          },
-          {
-            name: '李四',
-            age: 24,
-            address: '杭州',
-          },
-          {
-            name: '王五',
-            age: 25,
-            address: '上海',
-          },
-        ]
-      };
-    return {...state, ...mock, };
+    querySuccess(state, action){
+    //  const mock = {
+    //     total: 3,
+    //     current: 1,
+    //     loading: false,
+    //     list: [
+    //       {
+    //         name: '张三',
+    //         age: 23,
+    //         address: '成都',
+    //       },
+    //       {
+    //         name: '李四',
+    //         age: 24,
+    //         address: '杭州',
+    //       },
+    //       {
+    //         name: '王五',
+    //         age: 25,
+    //         address: '上海',
+    //       },
+    //     ]
+    //   };
+    return {...state, ...action.payload, loading: false};
     },
     createSuccess(){},
     deleteSuccess(){},
